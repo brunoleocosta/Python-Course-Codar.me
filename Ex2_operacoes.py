6f5780ac6ef6155f52c98b45143892c0588fd095
brunoleocosta <brunoleonardofc@gmail.com>
2025-01-27 14:02:19 -0500

    atualizacao

--- Ex2_operacoes.py
+++ Ex2_operacoes.py
@@ -4,4 +4,4 @@ x=True
 y=False
 
 print((x or y) and (a < b))
-print((x or y) and (a < b))
+print((x or y) and not (a < b))
---
+++ mercado.py
@@ -0,0 +1,4 @@
+valor_compra = float (input ("Digite o valor da compra:\n"))
+valor_frete = float (input ("Digite o valor do frete:\n"))
+fidelidade = bool(input ("é inscrito no programa de fidelidade?:\n"))
+if valor_compra > 100 or fidelidade ==True
