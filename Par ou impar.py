c5c766004efe52da98c4793dd3e69c5b7e2d8c13
brunoleocosta <brunoleonardofc@gmail.com>
2025-01-27 13:47:18 -0500

    Create Par ou impar.py

---
+++ Par ou impar.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número inteiro: "))
+
+# Verifica se o número é par ou ímpar e imprime o resultado
+if numero % 2 == 0:
+    print(True)  # Número é par
+else:
+    print(False)  # Número é ímpar5
+    
